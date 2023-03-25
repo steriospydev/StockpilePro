2f81dfdbd314d6be38aed36a8495ad01b68754b9
sterios <steriospydev@gmail.com>
2023-03-26 01:21:29 +0200

    add cancel button in stock-update

--- apps/storehouse/urls.py
+++ apps/storehouse/urls.py
@@ -9,9 +9,12 @@ urlpatterns = [
          views.storehouse_home, name='storehouse-main'),
     path('<int:pk>/',
          views.storage_bins_page, name="storage-detail"),
-    path('stock/', views.StockList.as_view(), name="stock-list"),
+    path('stock/',
+         views.StockList.as_view(), name="stock-list"),
     path('search/',
          views.StockSearchView.as_view(), name='stock-search'),
-    path('stock/<int:pk>/', views.StockDetailView.as_view(), name='stock-detail'),
-    path('stock/<int:pk>/edit/', views.StockUpdateView.as_view(), name='stock-update')
+    path('stock/<int:pk>/',
+         views.StockDetailView.as_view(), name='stock-detail'),
+    path('stock/<int:pk>/edit/',
+         views.StockUpdateView.as_view(), name='stock-update')
 ]
