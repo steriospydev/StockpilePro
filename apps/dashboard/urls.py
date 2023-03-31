53f4c9ec363f84ee9fac484a2c5d2a778d272e71
sterios <steriospydev@gmail.com>
2023-03-31 20:00:45 +0300

    Create report page

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -7,5 +7,6 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', views.index, name='index'),
     path('todo/delete/<int:item_id>/', views.remove, name='todo-remove'),
-    path('todo/<int:item_id>/change/', views.change_status, name='todo-change')
+    path('todo/<int:item_id>/change/', views.change_status, name='todo-change'),
+    path('report/', views.ops_report, name='report')
 ]
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
+from django.db.models import Sum
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .forms import TodoForm
 from .models import Todo
 
-from ..invoice.models import Invoice
+from ..invoice.models import Invoice, InvoiceItem
 from ..product.models import Product
 from ..supplier.models import Supplier
-from ..storehouse.models import Storage
+from ..storehouse.models import Storage, Stock
 
 @login_required
 def index(request):
@@ -49,3 +50,17 @@ def change_status(request, item_id):
         item.completed = True
     item.save()
     return redirect('dashboard:index')
+
+def ops_report(request):
+    product_sold = Stock.objects.aggregate(Sum('retrieved'))['retrieved__sum'] or 0
+    supplier_info = Invoice.objects.values('supplier__company').annotate(subtotal=Sum('subtotal'), total_taxes=Sum('total_taxes'))
+    product_in_storages = Stock.objects.aggregate(total=Sum('start_quantity') - Sum('retrieved'))['total'] or 0
+    most_retrieved_product = Stock.objects.values('item__product__product_name').annotate(total_item_sold=Sum(
+        'retrieved')).order_by('-total_item_sold')[:5]
+
+    context = {
+        'product_sold': product_sold,
+        'supplier_info': supplier_info,
+        'product_in_storages': product_in_storages,
+        'most_retrieved_product': most_retrieved_product}
+    return render(request, 'dashboard/report.html', context)
